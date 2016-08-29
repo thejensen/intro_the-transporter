7fbec0de2d26b8fef37c785070a52b77925a1857
Dean Scelza, Josh Huffman, Sara Jensen and Tim Bourgault <sara@>
2016-08-29 11:27:41 -0700

    add function to change background image

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var pictureArray = ["url('../img/picture1.jpg')", "url('../img/picture2.jpg')", "url('../img/picture3.jpg')"];
+var colorArray = ["brown", "blue", "red"];
+function changeDisplay(DOMElement, pictureArray, id){
+  $(DOMElement).css("background-color", pictureArray[id]); //change background-color to background-image once we have images
+}
+
 $(document).ready(function() {
 
 });
