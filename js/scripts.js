378eb103af45287f6cbadfadd98bd5290fedd935
Dean Scelza, Josh Huffman, Sara Jensen and Tim Bourgault <dean@>
2016-08-29 11:56:09 -0700

    add images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
+var pictureArray = ["url('../img/picture1.jpg')", "url('../img/picture2.jpg')", "url('../img/picture3.jpg')"];
+var colorArray = ["brown", "blue", "red"];
+function changeDisplay(DOMElement, pictureArray, id){
+  $(DOMElement).css("background-color", pictureArray[id]); //change background-color to background-image once we have images
+}
 
+$(document).ready(function() {
+  $("#switchColor").click(function(){
+    if ($(".console").css("background-color") === "grey"{
+      $(".console").css("background-color", "red");
+    } else {
+      $(".console").css("background-color", "grey");
+    }
+  });
 });
