5d1286bc62799303559e2e884f6c406a90c2dbc9
Dean Scelza, Josh Huffman, Sara Jensen and Tim Bourgault <tim@>
2016-08-31 09:50:15 -0700

    move to new branch, refactor communications

--- js/scripts.js
+++ js/scripts.js
@@ -3,54 +3,85 @@
 var spaceProps = {
   background : "url('./img/empty-space.jpg')",
   consoleColor : ["blue", "black", "purple"],
-  response : "You are now in control of the spaceship. Pull the lever to choose where to go"
+  response : function(clickCounter, name) {
+    if (clickCounter === 1) {
+      return "Hello there, " + name + ".";
+    } else if (clickCounter === 2) {
+      return "Welcome to the Teleporter. Your presence has been documented. You may attempt to command the ship. Trial and error is the most efficient plan your species has ever used to manipulate your environment. Do so now."
+    } else if (clickCounter === 3) {
+      return "Stop pushing this button " + name + ". There will be severe repercussions if you fail to comply. Try some of the other buttons."
+    } else {
+      return name + ". You have no friends."
+    }
+  }
 }
 var prehistoricProps = {
   background : "url('./img/prehistoric background.jpg')",
   consoleColor : ["red", "yellow", "orange"],
-  response : "You have landed with the dinosaurs.  Watch out!"
+  response : function(clickCounter, name) {
+    if (clickCounter === 1) {
+      return "You seem to be in prehistoric times, " + name + ". Watch out, you are nowhere near the top of the food chain here."
+    } else if (clickCounter === 2) {
+      return "I know you are insecure about the void, but you are less adept at predicting what will happen to you here. My button will not save you."
+    } else if (clickCounter === 3) {
+      return "It is probably time to move along, " + name + ". There is not much more to do here. Press the teleport button and see where you end up next.  Maybe it will be better, maybe it will be worse."
+    } else {
+      return "Much worse."
+    }
+  }
 }
 var futureProps = {
   background : "url('./img/futureCity1.jpg')",
   consoleColor : ["#bbbb77", "#006600", "#178282"],
-  reponse : "You are now in the future."
-}
-var allProps = [spaceProps, prehistoricProps, futureProps];
-
-// handles input from textbox
-function greeting(clickCounter, location, inputtedName) {
-  if (location === 0){
-    if (clickCounter === 1) {
-      return "Hello there, " + inputtedName + ".";
-    } else if (clickCounter === 2) {
-      return "Welcome to the Teleporter. Your presence has been documented. You may attempt to command the ship. Trial and error is the most efficient plan your species has ever used to manipulate your environment. Do so now."
-    } else if (clickCounter === 3) {
-      return "Stop pushing this button " + inputtedName + ". There will be severe repercussions if you fail to comply. Try some of the other buttons."
-    } else {
-      return inputtedName + ". You have no friends."
-    }
-  } else if (location === 1) {
-    if (clickCounter === 1) {
-      return "You seem to be in prehistoric times, " + inputtedName + ". Watch out, you are nowhere near the top of the food chain here."
-    } else if (clickCounter === 2) {
-      return "I know you are insecure about the void, but you are less adept at predicting what will happen to you here. My button will not save you."
-    } else if (clickCounter === 3) {
-      return "It is probably time to move along, " + inputtedName + ". There is not much more to do here. Press the teleport button and see where you end up next.  Maybe it will be better, maybe it will be worse."
-    } else {
-      return "Much worse."
-    }
-  } else if (location === 2) {
+  response : function(clickCounter, name) {
     if (clickCounter === 1) {
       return "You have arrived."
     } else if (clickCounter === 2) {
-      return "Welcome to the future. Is it everything you hoped for, " + inputtedName + "? Don't worrry, the flying cars make up for the unbreatheable atmosphere.";
+      return "Welcome to the future. Is it everything you hoped for, " + name + "? Don't worrry, the flying cars make up for the unbreatheable atmosphere.";
     } else if (clickCounter === 3) {
-      return "If it were possible for you to evacuate this Teleporter, " + inputtedName + ", I would recommend it. There's a great coffee shop just down the street. But you can't, so..."
+      return "If it were possible for you to evacuate this Teleporter, " + name + ", I would recommend it. There's a great coffee shop just down the street. But you can't, so..."
     } else {
       return "Keep on clicking if you want, but you are all alone now."
     }
   }
 }
+
+var allProps = [spaceProps, prehistoricProps, futureProps];
+
+// handles input from textbox
+// function greeting(clickCounter, location, inputtedName) {
+//   if (location === 0){
+//     if (clickCounter === 1) {
+//       return "Hello there, " + inputtedName + ".";
+//     } else if (clickCounter === 2) {
+//       return "Welcome to the Teleporter. Your presence has been documented. You may attempt to command the ship. Trial and error is the most efficient plan your species has ever used to manipulate your environment. Do so now."
+//     } else if (clickCounter === 3) {
+//       return "Stop pushing this button " + inputtedName + ". There will be severe repercussions if you fail to comply. Try some of the other buttons."
+//     } else {
+//       return inputtedName + ". You have no friends."
+//     }
+//   } else if (location === 1) {
+//     if (clickCounter === 1) {
+//       return "You seem to be in prehistoric times, " + inputtedName + ". Watch out, you are nowhere near the top of the food chain here."
+//     } else if (clickCounter === 2) {
+//       return "I know you are insecure about the void, but you are less adept at predicting what will happen to you here. My button will not save you."
+//     } else if (clickCounter === 3) {
+//       return "It is probably time to move along, " + inputtedName + ". There is not much more to do here. Press the teleport button and see where you end up next.  Maybe it will be better, maybe it will be worse."
+//     } else {
+//       return "Much worse."
+//     }
+//   } else if (location === 2) {
+//     if (clickCounter === 1) {
+//       return "You have arrived."
+//     } else if (clickCounter === 2) {
+//       return "Welcome to the future. Is it everything you hoped for, " + inputtedName + "? Don't worrry, the flying cars make up for the unbreatheable atmosphere.";
+//     } else if (clickCounter === 3) {
+//       return "If it were possible for you to evacuate this Teleporter, " + inputtedName + ", I would recommend it. There's a great coffee shop just down the street. But you can't, so..."
+//     } else {
+//       return "Keep on clicking if you want, but you are all alone now."
+//     }
+//   }
+// }
 
 // switches color of console from grey to red -- ******Will need to update with frontend changes to console layout*******
 function switchColor(color) {
@@ -79,33 +110,30 @@ $(document).ready(function() {
   $("#teleporter").click(function() {
     $("body").css("background-image", "url('./img/hyperspace2.gif')");
 
-    var hyperspeed = setTimeout(postHyperspeed, 2000);
+    var hyperspeed = setTimeout(postHyperspeed, 1950);
     function postHyperspeed() {
       clickCounter = 0;
-      if (location === 0) {
-        location = 1
-      } else if (location === 1) {
-        location = 2
-      } else if (location === 2) {
-        location = 0
+      if (location === allProps.length - 1) {
+        location = 0;
+      } else {
+        location ++;
+      }
      }
      $("body").css("background-image", allProps[location]["background"]);
-   }
    hyperspeed;
  });
 
 
-
-
-
 // let the alien communications begin
   $("#communicator").click(function() {
+    console.log("button clicked");
     if ($("#name").val() === "") {
       alert("To whom am I speaking?");
     } else {
       clickCounter += 1;
       var inputtedName = $("#name").val();
-      alert(greeting(clickCounter, location, inputtedName));
+      console.log(allProps[location]["response"](clickCounter, inputtedName))
+      alert(allProps[location]["response"](clickCounter, inputtedName));
     }
   });
 // pops up that goat
